35b890e1340c86909520682cb12760d192ddca82
Karen <karen.sauce@hotmail.com>
2023-05-31 23:01:10 -0300

    PracticoN0finalizado

---
+++ practico0/promedio3-tp1 .js
@@ -0,0 +1,34 @@
+let name = alert("Alumna: Karen Magali Sauce -- Actividad 4")
+
+//Punto a//
+
+let num4 = 5; 
+let num5 = 7; 
+let num6 = 9; 
+
+
+//Punto b//
+
+let num7 = -2; 
+let num8 = -3; 
+let num9 = -8; 
+
+//Punto c//
+
+let num10 = -1.2; 
+let num11 = 5.3; 
+let num12 = -3.3; 
+
+let constante = 3;
+
+let promedio= (num4 + num5 + num6) / constante; 
+
+console.log(promedio);
+
+let promedio1= (num7 + num8 + num9) / constante; 
+
+console.log(promedio1);
+
+let promedio2= (num10 + num11 + num12) / constante; 
+
+console.log(promedio2);
