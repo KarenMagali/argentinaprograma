5d3ff2c2a2d5a338ff2233f020226e4820c3849b
Karen <karen.sauce@hotmail.com>
2023-07-04 18:23:32 -0300

    practico4

---
+++ practico4/piedraPapelTijeras.js
@@ -0,0 +1,71 @@
+
+function obtenerJugadaComputadora(){ 
+
+const opciones = ["piedra", "papel", "tijera"];
+
+const sujuego = (Math.floor(Math.random() * opciones.length));
+
+return opciones[sujuego];
+
+}
+
+function  obtenerJugadaUsuario(){
+
+    const readlineSync = require('readline-sync'); 
+
+    const misopciones =["piedra", "papel", "tijera"];
+    
+    let numero = readlineSync.question("Ingrese: 1 = piedra, 2 = papel, 3 = tijeras");
+        
+    
+    if(numero>=1 && numero<=3){   
+    
+        let miopcion = misopciones[numero - 1];
+        return miopcion;
+
+    } else {
+
+        return "Opción inválida";
+
+    }    
+
+}
+
+
+function determinarGanador(){ 
+
+    const computadora = obtenerJugadaComputadora();
+    const usuario = obtenerJugadaUsuario();
+
+    if ((usuario == "piedra" && computadora == "papel")  || (usuario == "papel"  && computadora == "tijera") || (usuario == "tijera" && computadora == "piedra") 
+    
+    ){
+
+return "Gana la computadora"; 
+        
+
+    } else if (usuario == computadora) { 
+
+        return "Empate"; 
+
+    } else if (usuario == "Opción inválida") {
+        
+        return "No es posible ejecutar esta partida";
+
+    }else {
+        
+    return "Gana el usuario";
+
+    }
+
+}
+
+
+const c = obtenerJugadaComputadora();
+const u = obtenerJugadaUsuario();
+const ganador = determinarGanador();
+
+console.log("La computadora eligió: ", c);
+console.log("El usuario eligió ", u);
+console.log("El resultado fue ", ganador);
+
